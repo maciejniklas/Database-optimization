0ab4e23f0e1e887ba84ff2a679989fa22181a723
Maciej Niklas <kontakt@mniklas.pl>
2020-05-11 19:57:37 +0200

    Pessimistic  concurrency example

---
+++ Report_2/OracleMultithreadingTest/Users/BreweryOwner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace OracleMultithreadingTest.Users
+{
+    class BreweryOwner : AbstractUser
+    {
+        private string[] color = {
+            "Alice Blue", "Antique White", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "Blanched Almond", "Blue", "Blue Violet", "Brown", "Burly Wood", "Cadet Blue", "Chartreuse", "Chocolate", "Coral", "Cornflower Blue", "Cornsilk", "Crimson", "Cyan", "Dark Blue", "Dark Cyan", "Dark Golden Rod", "Dark Gray", "Dark Grey", "Dark Green", "Dark Khaki", "Dark Magenta", "Dark Olive Green", "Dark Orange", "Dark Orchid", "Dark Red", "Dark Salmon", "Dark Sea Green", "Dark Slate Blue", "Dark Slate Gray", "Dark Slate Grey", "Dark Turquoise", "Dark Violet", "Deep Pink", "Deep Sky Blue", "Dim Gray", "Dim Grey", "Dodger Blue", "Fire Brick", "Floral White", "Forest Green", "Fuchsia", "Gainsboro", "Ghost White", "Gold", "Golden Rod", "Gray", "Grey", "Green", "Green Yellow", "Honey Dew", "Hot Pink", "Indian Red", "Indigo", "Ivory", "Khaki", "Lavender", "Lavender Blush", "Lawn Green", "Lemon Chiffon", "Light Blue", "Light Coral", "Light Cyan", "Light Golden Rod Yellow", "Light Gray", "Light Grey", "Light Green", "Light Pink", "Light Salmon", "Light Sea Green", "Light Sky Blue", "Light Slate Gray", "Light Slate Grey", "Light Steel Blue", "Light Yellow", "Lime", "Lime Green", "Linen", "Magenta", "Maroon", "Medium Aqua Marine", "Medium Blue", "Medium Orchid", "Medium Purple", "Medium Sea Green", "Medium Slate Blue", "Medium Spring Green", "Medium Turquoise", "Medium Violet Red", "Midnight Blue", "Mint Cream", "Misty Rose", "Moccasin", "Navajo White", "Navy", "Old Lace", "Olive", "Olive Drab", "Orange", "Orange Red", "Orchid", "Pale Golden Rod", "Pale Green", "Pale Turquoise", "Pale Violet Red", "Papaya Whip", "Peach Puff", "Peru", "Pink", "Plum", "Powder Blue", "Purple", "Rebecca Purple", "Red", "Rosy Brown", "Royal Blue", "Saddle Brown", "Salmon", "Sandy Brown", "Sea Green", "Sea Shell", "Sienna", "Silver", "Sky Blue", "Slate Blue", "Slate Gray", "Slate Grey", "Snow", "Spring Green", "Steel Blue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "White Smoke", "Yellow", "Yellow Green"
+        };
+        private string[] item = { "Bootle", "Can", "Cap", "Keg", "Mug", "Opener", "Box", "Kettle" };
+        private string[] company = { "Brewery", "Firm", "Company", "Business", "Corporation", "Agency", "Institution", "Organization", "Concern", "Brewhouse" };
+        private Random random;
+        private string breweryName;
+
+        public BreweryOwner() : base() 
+        {
+            random = new Random();
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(color[random.Next(color.Length)]);
+            builder.Append(' ');
+            builder.Append(item[random.Next(item.Length)]);
+            builder.Append(' ');
+            builder.Append(company[random.Next(company.Length)]);
+
+            breweryName = builder.ToString();
+        }
+
+        public override void StartWork()
+        {
+            Communicate("I'm out of money and cannot pay my bills!");
+            Communicate("Let me change my brewery name in database. I think that anyone find my brewery then.");
+            Communicate("But first coffee");
+
+            Thread.Sleep(2000);
+
+            database.Modify("UPDATE Brewery SET breweryName = :param0 WHERE breweryID = 1501", new string[] { breweryName });
+
+            Communicate("It's done. My brewery is safe with name " + breweryName);
+        }
+    }
+}
---
+++ Report_2/OracleMultithreadingTest/Users/DatabaseAdmin.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Threading;
+
+namespace OracleMultithreadingTest.Users
+{
+    class DatabaseAdmin : AbstractUser
+    {
+        public DatabaseAdmin() : base() { }
+
+        public override void StartWork()
+        {
+            Communicate("One not paid order!");
+            Communicate("Right then, check name of this industry");
+
+            DataTable data = database.Read("SELECT breweryName FROM Brewery WHERE breweryId = 1501");
+
+            string breweryName = string.Empty;
+
+            foreach(DataRow row in data.Rows)
+            {
+                foreach(DataColumn column in data.Columns)
+                {
+                    if(column.ColumnName == "BREWERYNAME")
+                    {
+                        breweryName = row[column].ToString();
+                    }
+                }
+            }
+
+            Communicate("It is " + breweryName);
+            Communicate("Time for a sip of a coffee");
+
+            Thread.Sleep(1000);
+
+            Communicate("I should delete this man for his debt");
+
+            database.Modify("DELETE FROM Brewery WHERE breweryName = :param0", new string[] { breweryName });
+
+            Communicate("It's done");
+            Communicate("Wait a minute!");
+
+            data = database.Read("SELECT breweryName FROM Brewery WHERE breweryId = 1501");
+
+            breweryName = string.Empty;
+
+            foreach (DataRow row in data.Rows)
+            {
+                foreach (DataColumn column in data.Columns)
+                {
+                    if (column.ColumnName == "BREWERYNAME")
+                    {
+                        breweryName = row[column].ToString();
+                    }
+                }
+            }
+
+            Communicate("It changed name to " + breweryName);
+        }
+    }
+}
